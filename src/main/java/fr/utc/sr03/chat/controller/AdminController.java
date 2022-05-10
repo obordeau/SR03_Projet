c5c55af38d38c45678ef8d3e1ca9c0ca521fdd66
oceane <oceane.bordeau@etu.utc.fr>
2022-05-10 16:29:17 +0200

    ca marche pas

--- src/main/java/fr/utc/sr03/chat/controller/AdminController.java
+++ src/main/java/fr/utc/sr03/chat/controller/AdminController.java
@@ -5,12 +5,10 @@ import fr.utc.sr03.chat.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("admin")
@@ -40,6 +38,21 @@ public class AdminController {
         user.setActive(0);
         user.setAdmin(1);
         userRepository.save(user);
+        return "home_admin";
+    }
+    @GetMapping("modifySelf/{id}")
+    public String getUserInformation(@PathVariable Long id, Model model)
+    {
+        User user = userRepository.getById(id);
+        System.out.println(user.getFirstName());
+        model.addAttribute("user", user);
+        return "modif_user";
+    }
+
+    @PostMapping("modifySelf")
+    public String modifyUserInformation(@ModelAttribute User user, Model model) {
+        System.out.println(user.getFirstName());
+        //user.updateUser(user.getId(), user.getFirstName());
         return "home_admin";
     }
 }
--- src/main/java/fr/utc/sr03/chat/dao/UserRepository.java
+++ src/main/java/fr/utc/sr03/chat/dao/UserRepository.java
@@ -2,6 +2,10 @@ package fr.utc.sr03.chat.dao;
 
 import fr.utc.sr03.chat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 
@@ -10,5 +14,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
     List<User> findByMail(String mail);
 
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE users SET firstname= :firstname WHERE id= :id")
+    static void updateFirstname(@Param(value = "firstname") String firstname, @Param(value = "id") long id) {
+    }
+
+    User getById(Long id);
     User findFirstByMailAndPassword(String mail, String password);
 }
